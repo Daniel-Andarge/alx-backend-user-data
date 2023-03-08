bd00c619bda6d8bb50f3626b1dbd86d6f9351366
Daniel-Andarge <andargedaniel90@gmail.com>
2023-03-08 00:56:51 -0800

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,3 @@
-
-EBS@IT-PC2 MINGW64 ~
-$ ssh 26832540717b@26832540717b.34dca203.alx-cod.online
-26832540717b@26832540717b.34dca203.alx-cod.online's password:
-root@26832540717b:/# cd alx-backend-user-data
-root@26832540717b:/alx-backend-user-data# cd 0x01-Basic_authentication
-root@26832540717b:/alx-backend-user-data/0x01-Basic_authentication# cd api/v1
-root@26832540717b:/alx-backend-user-data/0x01-Basic_authentication/api/v1# mkdir auth
-root@26832540717b:/alx-backend-user-data/0x01-Basic_authentication/api/v1# cd auth
-root@26832540717b:/alx-backend-user-data/0x01-Basic_authentication/api/v1/auth# cat>auth.py
 #!/usr/bin/env python3
 """ Module of Authentication
 """
@@ -19,16 +9,32 @@ class Auth:
     """ Class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method for requiring authentication """
-        if path is None or excluded_paths is None or not len(excluded_paths):
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        # Add slash to all cases for consistency
-        if path[-1] != '/':
-            path += '/'
-        if excluded_paths[-1] != '/':
-            excluded_paths += '/'
-        if path in excluded_paths:
-            return False
+
+        l_path = len(path)
+        if l_path == 0:
+            return True
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
